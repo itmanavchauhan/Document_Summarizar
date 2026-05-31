1dcaae01192067ceebbf394349f93ecd52ef47e2
Manav Chauhan <chauhanmanav90@gmail.com>
2026-05-31 16:00:58 +0530

    model move inside the function

--- utils/vector_store.py
+++ utils/vector_store.py
@@ -1,13 +1,11 @@
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
-
-embedding_model = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
-
-
 def store_in_chroma(chunks):
+    
+    embedding_model = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
 
     vector_db = Chroma.from_texts(
         texts=chunks,
